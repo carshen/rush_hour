972f2a4b16c212f66ac8b5519b9e439fea50f922
Billy <billy@BILLINGTON>
2014-10-14 15:58:50 -0700

    added a few moar

--- test_move_gen.hs
+++ test_move_gen.hs
@@ -9,9 +9,14 @@ tests = [
 	(pat3_match "BB-" , ["-BB"]),
 	(pat3_match "aa--" , ["-aa-"]),
 	(pat3_match "aa-bb-c" , ["-aabb-c","aa--bbc"]),
-	(pat3_match "aa-bbC-c" , ["-aabbC-c"]),
-	(pat3_match "-aa---bc" , ["--aa--bc"]),
-	(pat3_match "--aa--bc" , ["---aa-bc"])
+	(pat3_match "aa-bb-cc-dd" , ["-aabb-cc-dd","aa--bbcc-dd","aa-bb--ccdd"]),
+	(pat3_match "aa-bb-cc-dd-" , ["-aabb-cc-dd-","aa--bbcc-dd-","aa-bb--ccdd-","aa-bb-cc--dd"]),
+	(pat3_match "aa-bbC-c", ["-aabbC-c"]),
+	(pat3_match "aa----bc", ["-aa---bc"]),
+	(pat3_match "-aa---bc", ["--aa--bc"]),
+	(pat3_match "--aa--bc", ["---aa-bc"]),
+	(pat3_match "---aa-bc", ["----aabc"]),
+	(pat3_match "----aabc", [])
 	
 	]
 	
