55b7b00bb48498a84b0e6788cc66075657ec7e8c
Billy <billy@BILLINGTON>
2014-10-15 00:58:44 -0700

    Separate file for horizontal stuff. Added test cases for truck and left. Maybe I should have gone into forestry

--- test_move_gen.hs
+++ test_move_gen.hs
@@ -1,4 +1,5 @@
 import Solver
+import Horiz
 
 tests :: [([String],[String])]
 
@@ -8,10 +9,17 @@ e = moves_horiz
 f :: String -> [String]
 f = moves_right
 
+g :: String -> [String]
+g = moves_left
+
 tests = [
 	(e "" , []),
+	(e "aaabb", []),
 	(e "aa-", ["-aa"]),
 	(e "-aa", ["aa-"]),
+	(e "-aaa", ["aaa-"]),
+	(e "aaa-", ["-aaa"]),
+	(e "bbb-aaa-", ["-bbbaaa-","bbb--aaa","bbbaaa--"]),
 
 	(f "" ,[]),
 	(f "-" , []),
@@ -22,11 +30,22 @@ tests = [
 	(f "aa-bb-cc-dd" , ["-aabb-cc-dd","aa--bbcc-dd","aa-bb--ccdd"]),
 	(f "aa-bb-cc-dd-" , ["-aabb-cc-dd-","aa--bbcc-dd-","aa-bb--ccdd-","aa-bb-cc--dd"]),
 	(f "aa-bbC-c", ["-aabbC-c"]),
+	
+	-- to the right to the right
 	(f "aa----bc", ["-aa---bc"]),
 	(f "-aa---bc", ["--aa--bc"]),
 	(f "--aa--bc", ["---aa-bc"]),
 	(f "---aa-bc", ["----aabc"]),
-	(f "----aabc", [])
+	(f "----aabc", []),
+	
+	-- to the left to the left
+	(g "----aabc", ["---aa-bc"]),
+	(g "---aa-bc", ["--aa--bc"]),
+	(g "--aa--bc", ["-aa---bc"]),
+	(g "-aa---bc", ["aa----bc"]),
+	(g "aa----bc", []),
+	
+	(g "-aa-bb-cc-dd", ["aa--bb-cc-dd","-aabb--cc-dd","-aa-bbcc--dd", "-aa-bb-ccdd-"])
 	]
 	
 run_tests :: [([String],[String])] -> [String]
