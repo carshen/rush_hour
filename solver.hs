70e1a4aa1b5f5c3ec380618cb2109feb3fdbbc68
Billy <billy@BILLINGTON>
2014-10-15 03:18:01 -0700

    Generalize to multiple horizontal lines - maybe I should have been a hobo

--- solver.hs
+++ solver.hs
@@ -7,11 +7,14 @@ import Horiz
 -- Assumption: the board is at least a 3x2 matrix. Note we don't assume the board always has to be size 6,
 -- But it will do the job for a 6x6 matrix.
 
-rush_hour :: [String] -> [[String]]
+type Board = [String]
+type Line = String
+
+rush_hour :: Board -> [Board]
 rush_hour init_state = reverse(solver [init_state] [])
 
 -- Takes a list of unexplored boards, and the list of boards experienced so far. 
-solver :: [[String]] -> [[String]] -> [[String]]
+solver :: [Board] -> [Board] -> [Board]
 solver unexplored path
 	| null unexplored		= []
 	| elem (head unexplored) path	= solver (tail unexplored) path
@@ -21,24 +24,53 @@ solver unexplored path
 	where result = solver (generate_moves (head unexplored)) ((head unexplored):path)
 	
 -- Checks if 'XX' present in last 2 columns of the 3rd row on the board.
-is_goal :: [String] -> Bool
+is_goal :: Board -> Bool
 is_goal board = (special_car_at_goal (get_row board 3) (length (head board)))
 
 -- Takes a board and generates a list of boards 1 move away.
-generate_moves :: [String] -> [[String]]
+generate_moves :: Board -> [Board]
 generate_moves board = (generate_vertical_moves board) ++ (generate_horizontal_moves board)
 
 -- Takes a board and generates a list of boards where each board is a valid vertical move.
-generate_vertical_moves :: [String] -> [[String]]
+generate_vertical_moves :: Board -> [Board]
 generate_vertical_moves board = [board]
 
 -- Takes a board and generates a list of boards where each board is a valid horizontal move.
-generate_horizontal_moves :: [String] -> [[String]]
-generate_horizontal_moves board = [board]
+-- O(2^(nm)) where n is number of lines m is length of line
 
+-- Prepend a line to a board
+prepend_to_board :: Board -> Line -> Board
+prepend_to_board board line = line: board
+
+generate_horizontal_moves :: Board -> [Board]
+generate_horizontal_moves [] = []
+generate_horizontal_moves x = horizontal_moves_helper x
+
+horizontal_moves_helper :: Board -> [Board]
+horizontal_moves_helper [] = []
+horizontal_moves_helper (line:[])
+	| null line_moves = [[line]]
+	| otherwise = map (\line -> [line]) line_moves
+	where line_moves = moves_horiz line
+	
+horizontal_moves_helper (line:(next_line:rest)) 
+ 	| null line_moves = map (\ board -> prepend_to_board board line) next_boards
+	| otherwise =
+		concat
+		(
+		map
+			(\ line ->
+				map (\ board -> prepend_to_board board line) next_boards
+			)
+		line_moves
+		)
+ 	where
+ 		line_moves = moves_horiz line
+ 		next_boards = horizontal_moves_helper (next_line:rest)
+		
 -- Misc. helpers
 -- Return the nth row of the board.
-get_row :: [String] -> Int -> String
+get_row :: Board -> Int -> String
 get_row board n
 	| n==1		= (head board)
 	| otherwise	= (get_row (tail board) (n-1))
